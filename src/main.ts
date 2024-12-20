d7562691f4399f7076286c77f12161e699624e4f
hjortie <elinhjortskull@live.se>
2024-12-20 13:02:21 +0100

    services and models in place

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import { getClothes } from "./services/womensClothingService";
 import "./style.css";
+
+const clothes = await getClothes();
+
+clothes.forEach((item) => {
+  const image = document.createElement("img");
+  image.src = item.image;
+  image.alt = item.title;
+
+  document.getElementById("womens_clothing")?.appendChild(image);
+});
