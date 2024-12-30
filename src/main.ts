0cca59704eed6179b8cd75648d65d1b1e97cd0e6
hjortie <elinhjortskull@live.se>
2024-12-30 14:09:38 +0100

    map and new class w size and quantity properties

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {
   createHtmlMensClothes,
   createHtmlWomensClothes,
 } from "./helpers/htmlHelper";
+import { Article } from "./services/article";
 import { getMensClothes } from "./services/mensClothing";
 import { getClothes } from "./services/womensClothingService";
 import "./style.css";
@@ -9,5 +10,32 @@ import "./style.css";
 const womensClothes = await getClothes();
 const mensClothes = await getMensClothes();
 
-createHtmlWomensClothes(womensClothes);
-createHtmlMensClothes(mensClothes);
+const newWomensClothes = womensClothes.map(
+  (item) =>
+    new Article(
+      item.title,
+      item.price,
+      item.image,
+      item.description,
+      0,
+      false,
+      false,
+      false
+    )
+);
+
+const newMensClothes = mensClothes.map(
+  (item) =>
+    new Article(
+      item.title,
+      item.price,
+      item.image,
+      item.description,
+      0,
+      false,
+      false,
+      false
+    )
+);
+createHtmlWomensClothes(newWomensClothes);
+createHtmlMensClothes(newMensClothes);
