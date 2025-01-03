dcf3ae8e1bba61fd59f6b2e25b69b653a4035d9c
hjortie <elinhjortskull@live.se>
2025-01-03 12:51:02 +0100

    attempted data connection to modal

--- src/main.ts
+++ src/main.ts
@@ -39,3 +39,34 @@ const newMensClothes = mensClothes.map(
 );
 createHtmlWomensClothes(newWomensClothes);
 createHtmlMensClothes(newMensClothes);
+
+const cartItemContainer = document.getElementById(
+  "cart-items"
+) as HTMLTableElement;
+const cartItemQty = document.getElementById(
+  "qty-container"
+) as HTMLTableElement;
+const cartItems: Article[] = JSON.parse(localStorage.getItem("cart") || "[]");
+let total = 0;
+
+cartItems.forEach((item) => {
+  const itemName = document.createElement("p");
+  const itemPrice = document.createElement("p");
+  const itemQty = document.createElement("input");
+
+  itemPrice.className = "text-muted";
+  itemQty.type = "text";
+  itemQty.className = "form-control";
+
+  itemName.innerText = item.title;
+  itemPrice.innerText = item.price.toString();
+  //itemQty.value = item.quantity.toString();
+
+  cartItemContainer.appendChild(itemName);
+  cartItemContainer.appendChild(itemPrice);
+  cartItemQty.appendChild(itemQty);
+
+  const cost = document.getElementById("total-cost") as HTMLSpanElement;
+  total += item.price;
+  cost.innerText = `${total.toString()}$`;
+});
