ab96ba0d574ce4335f41309d3e9a5289b6de612e
hjortie <elinhjortskull@live.se>
2025-01-09 16:07:27 +0100

    a lil extra scrub

--- src/helpers/htmlHelper.ts
+++ src/helpers/htmlHelper.ts
@@ -112,31 +112,6 @@ export const createHtmlClothes = (clothes: Article[]) => {
   });
 };
 
-// export const createHtmlMensClothes = (clothes: Article[]) => {
-//   clothes.forEach((item) => {
-//     const itemContainer = document.createElement("div");
-//     const image = document.createElement("img");
-//     const title = document.createElement("span");
-//     const price = document.createElement("p");
-
-//     itemContainer.id = "item-container";
-//     image.src = item.image;
-//     image.alt = item.title;
-//     title.innerHTML = item.title;
-//     price.innerHTML = `${item.price} USD`;
-
-//     itemContainer.appendChild(image);
-//     itemContainer.appendChild(title);
-//     itemContainer.appendChild(price);
-//     document.getElementById("mens-clothing")?.appendChild(itemContainer);
-
-//     itemContainer.addEventListener("click", () => {
-//       localStorage.setItem("selectedProduct", JSON.stringify(item));
-//       window.location.href = "product-page.html";
-//     });
-//   });
-// };
-
 export function updateCartItemCount(count: number): void {
   const cartIcon = document.getElementById("ShoppingCart") as HTMLElement;
 
